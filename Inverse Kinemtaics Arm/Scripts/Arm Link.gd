80831eb5693e8006fad1b5e36860b30ab3c8cdbe
Parm345 <51205078+Parm345@users.noreply.github.com>
2022-06-03 20:01:40 -0400

    Working on 2 bar IK arm
    - have some code but it's buggy
    - fix bugs

---
+++ Inverse Kinemtaics Arm/Scripts/Arm Link.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var armLength:int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	armLength = $"Pointer End".position.length()
+#	print(armLength)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Inverse Kinemtaics Arm/Scripts/Main.gd
+++ Inverse Kinemtaics Arm/Scripts/Main.gd
@@ -5,13 +5,25 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+onready var link1 = $"Arm Link 1"
+onready var link2 = $"Arm Link 2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	link2.position = link1.get_node("Pointer End").global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mousePos = get_viewport().get_mouse_position()
-	print(mousePos)
+#	$"Arm Link 1".set_rotation(atan2(mousePos.y-$"Arm Link 1".position.y, mousePos.x-$"Arm Link 1".position.x )-deg2rad(90))
+	inverseKinematics(mousePos)
+
+func inverseKinematics(mousePos):
+	var desiredPos = mousePos - link1.position
+	var theta = atan2(desiredPos.y, desiredPos.x)
+	var phi1 = acos(pow(desiredPos.length(),2)/(2*link2.armLength*desiredPos.length())) # simplified cosine law 
+	link1.set_rotation(theta+phi1 -deg2rad(180))
+	var phi2 = (mousePos - link1.get_node("Pointer End").position).angle()
+	link2.position = link1.get_node("Pointer End").global_position
+	
